2e2dd4dda03248da425c08e36fafe4826605a539
Kofi Arhin <kofiarhin69@gmail.com>
2026-07-26 06:36:16 +0100

    Allow conventional unused middleware arguments

--- apps/server/eslint.config.js
+++ apps/server/eslint.config.js
@@ -5,7 +5,14 @@ export default [
   { ignores: ["coverage"] },
   {
     files: ["**/*.js"],
-    languageOptions: { ecmaVersion: 2022, sourceType: "module", globals: { ...globals.node, ...globals.jest } },
-    rules: { ...js.configs.recommended.rules },
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: { ...globals.node, ...globals.jest },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
   },
 ];
